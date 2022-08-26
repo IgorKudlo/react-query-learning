e115f3e4d73cce4b2807283cce6855aec26f5927
igor.kudlo <igor.kudlo@spiralscout.com>
2022-08-26 22:18:00 +0300

    devtools

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Films from './Films';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
@@ -10,6 +11,7 @@ function App() {
             <div className="App">
                 <Films/>
             </div>
+            <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     );
 }
